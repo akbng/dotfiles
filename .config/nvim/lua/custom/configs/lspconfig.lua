73d7f4d4415a7b6dded07ed6f24a6dff777befdd
akbng <91788199+akbng@users.noreply.github.com>
2023-12-01 21:47:38 +0530

    add: python lsp

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -9,7 +9,7 @@ capabilities.textDocument.foldingRange = {
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "emmet_ls", "tailwindcss" }
+local servers = { "html", "cssls", "tsserver", "clangd", "emmet_ls", "tailwindcss", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -1,7 +1,7 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-	return
+  return
 end
 
 local fmt = null_ls.builtins.formatting
@@ -9,43 +9,45 @@ local diag = null_ls.builtins.diagnostics
 local ca = null_ls.builtins.code_actions
 
 local sources = {
-	-- webdev stuff: [ js, ts, jsx, tsx, vue, css, scss, less, html, json, jsonc, yaml, md, mdx, graphql, handlebars]
-	fmt.prettierd,
-	-- Lua
-	fmt.stylua,
-	-- Dart
-	fmt.dart_format,
-	-- c, cpp, cs(c#), java, cuda, proto
-	fmt.clang_format,
-	-- shell
-	fmt.shfmt,
-	-- linters
-	diag.eslint_d,
-	diag.alex.with({
-		extra_filetypes = { "norg" },
-	}),
-	diag.shellcheck,
-	-- code actions
-	ca.gitsigns,
-	ca.eslint_d,
-	ca.shellcheck,
+  -- webdev stuff: [ js, ts, jsx, tsx, vue, css, scss, less, html, json, jsonc, yaml, md, mdx, graphql, handlebars]
+  fmt.prettierd,
+  -- Lua
+  fmt.stylua,
+  -- Dart
+  fmt.dart_format,
+  -- c, cpp, cs(c#), java, cuda, proto
+  fmt.clang_format,
+  -- shell
+  fmt.shfmt,
+  -- python
+  fmt.black,
+  -- linters
+  diag.eslint_d,
+  diag.alex.with {
+    extra_filetypes = { "norg" },
+  },
+  diag.shellcheck,
+  -- code actions
+  ca.gitsigns,
+  ca.eslint_d,
+  ca.shellcheck,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup({
-	border = "rounded",
-	sources = sources,
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
+null_ls.setup {
+  border = "rounded",
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format { bufnr = bufnr }
+        end,
+      })
+    end
+  end,
+}
